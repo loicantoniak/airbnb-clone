154026609c8fd35c754e063d60da4096b1c21194
Loïc Antoniak <loic.antoniak@gmail.com>
2020-10-05 09:17:59 +0200

    Updates

---
+++ precache-manifest.b06ef2f7c21a064674fd7bf5d0463979.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cfe77e19009487801144625358ac29d",
+    "url": "/airbnb-clone/index.html"
+  },
+  {
+    "revision": "0e4ef2da26111780e7d1",
+    "url": "/airbnb-clone/static/css/2.9229e6f1.chunk.css"
+  },
+  {
+    "revision": "bb726c0cf5721747867f",
+    "url": "/airbnb-clone/static/css/main.46fb333f.chunk.css"
+  },
+  {
+    "revision": "0e4ef2da26111780e7d1",
+    "url": "/airbnb-clone/static/js/2.0917b5c7.chunk.js"
+  },
+  {
+    "revision": "3ceb8c17f560e6682fd3523301e674b3",
+    "url": "/airbnb-clone/static/js/2.0917b5c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb726c0cf5721747867f",
+    "url": "/airbnb-clone/static/js/main.cf638f54.chunk.js"
+  },
+  {
+    "revision": "f358a9c4d5787a3a04f2",
+    "url": "/airbnb-clone/static/js/runtime-main.fc586be8.js"
+  }
+]);
